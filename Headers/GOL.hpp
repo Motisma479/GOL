4ac9d1c2a65501036b33148c96b88c0234a36794
m.robion <m.robion@student.isartdigital.com>
2023-10-18 16:27:59 +0200

    [modified]Reworked and now working

--- Headers/GOL.hpp
+++ Headers/GOL.hpp
@@ -1,29 +1,144 @@
 //Game of life header file
-#include <stddef.h>
 #include <chrono>
+#include <iostream>
+#include <string>
+#include <bitset>
 #include "terminal.h"
 
+#ifndef COLOR_ALIVE
+#define COLOR_ALIVE "0;0;0"
+#endif
+
+#ifndef COLOR_DEAD
+#define COLOR_DEAD "255;255;255"
+#endif
+
+#define __COLOR_ALIVE__ "\033[48;2;"COLOR_ALIVE"m"
+#define __COLOR_DEAD__ "\033[48;2;"COLOR_DEAD"m"
+
+template <int size_x, int size_y>
 class GOL
 {
 public:
     bool isFinished;
 
-    GOL() = delete;
-    GOL(int size_x, int size_y);
-    ~GOL();
+    GOL()
+    {
+        isFinished = false;
+        terminal.Init();
 
-    bool update(int delay);
-    void terminalPrint();
-    void changeCell(int x, int y);
+        for(int x = 0; x < size_x; x++)
+        {
+            for (int y = 0; y < size_y; y++)
+            {
+                data.set(x+y*size_x, false);
+            }
+        }
+
+        shouldUpdate = true;
+        firstUpdate = true;
+    }
+    ~GOL()
+    {
+
+    }
+
+    bool update(int delay)
+    {
+        if(firstUpdate)
+        {
+            waitStart = std::chrono::system_clock::now();
+            shouldUpdate = false;
+            firstUpdate = false;
+            return true;
+        }
+        if (shouldUpdate)
+        {
+            //copy data to tempData
+            auto tempData = std::bitset<size_x*size_y>(data);
+
+            //cells work
+            for (int x = 0; x < size_x; x++)
+            {
+                for (int y = 0; y < size_y; y++)
+                {
+                    tempData.set(x+y*size_x, updateCell(x,y));
+                }
+            }
+            
+            //copy tempData to data
+            data = tempData;
+
+            waitStart = std::chrono::system_clock::now();
+            shouldUpdate = false;
+            return true;
+        }
+        if (waitStart + std::chrono::milliseconds(delay) >= std::chrono::system_clock::now())
+            return false;   
+        shouldUpdate = true;
+        return false;
+    }
+    void terminalPrint()
+    {
+        terminal.goAt(0,0);
+        for (int y = 0; y < size_y; y++)
+        {
+            for (int x = 0; x < size_x; x++)
+            {
+                if (data[x+y*size_x])
+                    std::cout<< __COLOR_ALIVE__ << "  "; //color: 0, 0, 0 by default
+                else
+                    std::cout<< __COLOR_DEAD__ << "  "; // color: 255, 255, 255 by default
+            } 
+            std::cout<<std::endl;
+        }
+    }
+    void changeCell(int x, int y)
+    {
+        data.flip(x+y*size_x);
+    }
+
 private:
     Terminal terminal;
-    int sizeX, sizeY;
-    bool* data;
-    bool* tempData;
+    std::bitset<size_x*size_y> data;
+
     bool shouldUpdate;
     bool firstUpdate;
     std::chrono::system_clock::time_point waitStart;
     
-    bool getValue(int x, int y);
-    bool updateCell(int x, int y);
+    bool getValue(int x, int y)
+    {
+        if (x < 0) x += size_x;
+        if (y < 0) y += size_y;
+        if (x > size_x-1) x -= size_x;
+        if (y > size_y-1) y -= size_y;
+        return data[x+y*size_x];
+    }
+    bool updateCell(int x, int y)
+    {
+        int livingArround=0;
+
+        livingArround += getValue(x-1,y+1);
+        livingArround += getValue(x,y+1);
+        livingArround += getValue(x+1,y+1);
+        livingArround += getValue(x-1,y);
+
+        livingArround += getValue(x+1,y);
+        livingArround += getValue(x-1,y-1);
+        livingArround += getValue(x,y-1);
+        livingArround += getValue(x+1,y-1);
+
+        
+        if (data[x+y*size_x])
+        {
+            if (livingArround<2 || livingArround>3)
+                return false;
+        }
+        else
+        {
+            if (livingArround==3)
+                return true;
+        }
+        return data[x+y*size_x];
+    }
 };
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,39 +1,71 @@
+#define COLOR_ALIVE "159;223;76"
+#define COLOR_DEAD "37;27;44"
 #include "GOL.hpp"
 #include <iostream>
 
 int main()
 {
     std::system("chcp 65001");
-    GOL newWorld(17,16);
+    GOL<50, 50> newWorld;
+
+    // //-------------------------------------------------------------
+    // /*
+
+    //   ██
+    // ██  ██
+    // ██  ██
+    // ██  ██
+    //   ██
+
+    // */
+    // newWorld.changeCell(7,5);
+    // newWorld.changeCell(6,6);
+    // newWorld.changeCell(8,6);
+    // newWorld.changeCell(6,7);
+    // newWorld.changeCell(8,7);   
+    // newWorld.changeCell(6,8);
+    // newWorld.changeCell(8,8);
+    // newWorld.changeCell(7,9);
+    // //-------------------------------------------------------------
+
     //-------------------------------------------------------------
     /*
 
       ██
-    ██  ██
-    ██  ██
-    ██  ██
-      ██
+        ██
+    ██████
 
     */
     newWorld.changeCell(7,5);
-    newWorld.changeCell(6,6);
     newWorld.changeCell(8,6);
     newWorld.changeCell(6,7);
-    newWorld.changeCell(8,7);   
-    newWorld.changeCell(6,8);
-    newWorld.changeCell(8,8);
-    newWorld.changeCell(7,9);
-
-    //newWorld.changeCell(0,0);
-    //newWorld.changeCell(0,15);
-    //newWorld.changeCell(16,0);
-    //newWorld.changeCell(16,15);
+    newWorld.changeCell(7,7);
+    newWorld.changeCell(8,7);
     //-------------------------------------------------------------
+
+    // //-------------------------------------------------------------
+    // /*
+        
+    // ██
+    // ██████
+    //   ██
+
+    // */
+    // newWorld.changeCell(7,5);
+    // newWorld.changeCell(7,6);
+    // newWorld.changeCell(8,6);
+    // newWorld.changeCell(9,6);
+    // newWorld.changeCell(8,7);
+    // //-------------------------------------------------------------
+
+    newWorld.terminalPrint(); //Print the first time
     while (newWorld.isFinished == false)
     {
-        if(newWorld.update(1000))
+        if(newWorld.update(50))
             newWorld.terminalPrint();
     }
+
+    system("pause");
 
     return 0;
 }
